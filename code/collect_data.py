bdea7d86773c14aeb0f50663701c27aeb63e6d64
samor <dingurth.d@gmail.com>
2018-01-27 14:13:44 +0100

    first training aproach'

---
+++ code/collect_data.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 18 15:45:36 2018
+
+Using
+Animals with Attributes Dataset, http://attributes.kyb.tuebingen.mpg.de
+(C) 2009 Christoph Lampert <chl@tuebingen.mpg.de>
+
+@author: dominik
+"""
+
+import numpy as np
+
+import _pickle as cPickle
+import bz2
+
+def nameonly(x):
+    return x.split('\t')[1]
+
+def loadstr(filename, converter=str):
+    return [converter(c.strip()) for c in open(filename).readlines()]
+
+def bzUnpickle(filename):
+    return cPickle.load(bz2.BZ2File(filename))
+
+# adapt these paths and filenames to match local installation
+
+local_path = '/home/dominik/Dokumenty/Studia/Data-mining/Animals-with-Attributes/'
+feature_pattern = local_path + 'code/feat/%s-%s.pic.bz2'
+labels_pattern =  local_path + 'code/feat/%s-labels.pic.bz2'
+
+all_features = ['cq','lss','phog','sift','surf','rgsift']
+
+features_length = {}
+
+attribute_matrix = 2*np.loadtxt(local_path + 'predicate-matrix-binary.txt', dtype=float)-1
+classnames = loadstr(local_path + 'classes.txt', nameonly)
+attributenames = loadstr(local_path + 'predicates.txt', nameonly)
+
+train_classes = loadstr(local_path + 'trainclasses.txt')
+test_classes = loadstr(local_path + 'testclasses.txt')
+
+def createData(all_classes, attribute_id):
+    """ create set of data from all statistics """
+    featurehist = {}
+    for feature in all_features:
+        featurehist[feature] = []
+    
+    labels = []
+    for classname in all_classes:
+        for feature in all_features:
+            featurefilename = feature_pattern % (classname,feature)
+            print('# ',featurefilename)
+            histfile = bzUnpickle(featurefilename)
+            featurehist[feature].extend( histfile )
+        
+        labelfilename = labels_pattern % classname
+        print('# ',labelfilename)
+        print('#')
+        labels.extend( bzUnpickle(labelfilename)[:,attribute_id] )
+    
+    for feature in all_features:
+        temp = np.array(featurehist[feature])
+        featurehist[feature] = (temp - temp.mean(axis=0)) / np.maximum(temp.std(axis=0),1)
+    
+    labels = np.array(labels)
+
+    for feature in all_features:
+        features_length[feature] = featurehist[feature].shape[1]
+
+    return featurehist,labels
+
+
+def flattenDataSet(dataSet):
+    data = np.concatenate([dataSet['cq'],dataSet['lss'],dataSet['phog'], dataSet['sift'], dataSet['surf'], dataSet['rgsift']], axis=1)
+    return data
+
+
---
+++ code/train_attribute.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 26 17:53:36 2018
+
+Using
+Animals with Attributes Dataset, http://attributes.kyb.tuebingen.mpg.de
+(C) 2009 Christoph Lampert <chl@tuebingen.mpg.de>
+
+@author: dominik
+"""
+
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.metrics.pairwise import chi2_kernel
+import collect_data as cd
+
+import _pickle as cPickle
+import bz2
+
+def bzPickle(obj,filename):
+    f = bz2.BZ2File(filename, 'wb')
+    cPickle.dump(obj, f)
+    f.close()
+
+outputSVM_pattern =  './results/%s-SVM.pic.bz2'
+
+gamma = np.ones(6)  
+
+def statKernel(x1, x2):
+    cqKernel = chi2_kernel(x1[:cd.features_length['cq']], x2[:cd.features_length['cq']], gamma=gamma[0])
+
+    lssKernel = chi2_kernel(x1[cd.features_length['cq']:cd.features_length['lss']], x2[cd.features_length['cq']:cd.features_length['lss']], gamma=gamma[1])
+
+    phogKernel = chi2_kernel(x1[cd.features_length['lss']:cd.features_length['phog']], x2[cd.features_length['lss']:cd.features_length['phog']], gamma=gamma[2])
+
+    siftKernel = chi2_kernel(x1[cd.features_length['phog']:cd.features_length['sift']], x2[cd.features_length['phog']:cd.features_length['sift']], gamma=gamma[3])
+
+    surfKernel = chi2_kernel(x1[cd.features_length['sift']:cd.features_length['surf']], x2[cd.features_length['sift']:cd.features_length['surf']], gamma=gamma[4])
+
+    rgsiftKernel = chi2_kernel(x1[cd.features_length['surf']:cd.features_length['rgsift']], x2[cd.features_length['surf']:cd.features_length['rgsift']], gamma=gamma[5])
+
+    kernels = np.array([cqKernel, lssKernel, phogKernel, siftKernel, surfKernel, rgsiftKernel])
+
+    return np.sum(kernels)
+
+def trainAttribute(attributeId):
+    data, labels = cd.createData(cd.train_classes, attributeId)
+    trainData = cd.flattenDataSet(data)
+    
+    svm = SVC(C=10., kernel=chi2_kernel, probability=True)
+    svm.fit(trainData, labels)
+
+    filename = outputSVM_pattern % cd.attributenames[attributeId]
+    bzPickle(svm, filename)
+    
